1fd96e7e33bbe1ee1f5293a0c9f94d7ec0d7ca0b
Florence-Njeri <flonsie17@gmail.com>
2023-07-06 07:59:15 +0300

    add CRUD actions for the alpha blog as well as the MVC logic for the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: [:show, :edit, :update, :destroy ]
 
   # GET /articles or /articles.json
   def index
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    
   end
 
   # GET /articles/new
@@ -15,52 +16,50 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  # GET /articles/1/edit
-  def edit
-  end
-
-  # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save!
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
     end
   end
 
+  # GET /articles/1/edit
+  def edit
+    
+
+  end
+
+  # POST /articles or /articles.json
+  
+
+
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    
+    if @article.update(article_params)
+      flash[:notice] = "Article with id #{params[:id]} was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
+    flash[:notice] = "Article with id #{params[:id]} was updated successfully"
+    redirect_to articles_path
 
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
-
+ 
+# Methods only available in this controller
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+    # Validate that user can't save article without a title
+    # Enforce minimum length requirements for the fields
+    validates :title, presence:true, length: {minimum: 6, maximum: 100 } 
+    validates :description, presence:true, length: {minimum: 10, maximum: 100 } 
+    
 end
