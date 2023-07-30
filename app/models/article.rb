06b5ae29b867cc55cf04d15cfeeddbdbaa8ab349
Florence-Njeri <flonsie17@gmail.com>
2023-07-30 18:13:24 +0300

    add association between a user and articles and update all the articles in the db to belong to the firts user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
+    belongs_to :user
     # Validate that user can't save article without a title
     # Enforce minimum length requirements for the fields
     validates :title, presence:true, length: {minimum: 6, maximum: 100 } 
     validates :description, presence:true, length: {minimum: 10, maximum: 100 } 
-    
 end
