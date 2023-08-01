3aa2855dd97e7aa588d91c18307ee8042fc417e6
Florence-Njeri <flonsie17@gmail.com>
2023-08-01 09:17:37 +0300

    add users create action and fix the validation errors not showing up due to data turbo in rails 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@ class UsersController < ApplicationController
     end
 
     def create
+        # byebug
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to Alpha Blog #{@user.username}, You have successfully signed up!"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
     end
 
 end
